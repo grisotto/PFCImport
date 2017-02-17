2c8f9e247281a115ed7699625d989b2a7121fec8
Rafael Grisotto <grisotto18@gmail.com>
2017-02-17 00:08:39 -0200

    Pequenas correcoes

--- src/Dataset.h
+++ src/Dataset.h
@@ -2,10 +2,12 @@
 #ifndef DATASET_H_
 #define DATASET_H_
 #define GET_CELL(h, i,j) (&((h)->matriz[ (i) * (h)->qtd_linhas + (j)]))
-const int MaxLin = 200;
-const int MaxCol = 200;
-const double limitanteLargura = 0.225792;
-const double limitanteAltura = 0.195139  ;
+//AQUILIMITESELIMITANTE
+
+//const int MaxLin = 200;
+//const int MaxCol = 200;
+//const double limitanteLargura = 0.225792;
+//const double limitanteAltura = 0.195139  ;
 
 
 
